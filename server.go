daea6a41d6bb1f66e3f01df5208e937b422388a2
密码0000 <821505662@qq.com>
2025-06-28 14:22:42 +0800

    version 2 增加了广播和用户端

--- server.go
+++ server.go
@@ -3,39 +3,70 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	OnlineMap map[string]*User
+	maplock   sync.RWMutex
+
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 
 }
-func (this *Server) Handler(conn net.Conn) {
-	fmt.Println("Linked Succeeded")
+func (now *Server) ListenMessager() {
+	for {
+		msg := <-now.Message
+		now.maplock.Lock()
+		for _, cil := range now.OnlineMap {
+			cil.C <- msg
+		}
+		now.maplock.Unlock()
+	}
 }
-func (this *Server) Start() {
+func (now *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	now.Message <- sendMsg
+}
+func (now *Server) Handler(conn net.Conn) {
+	//fmt.Println("Linked Succeeded")
+	//用户上线 加map
+	user := NewUser(conn)
+	now.maplock.Lock()
+	now.OnlineMap[user.Name] = user
+	now.maplock.Unlock()
+
+	//广播信息
+	now.BroadCast(user, "上线")
+}
+func (now *Server) Start() {
 	fmt.Println("Start() 已运行") // 👈 先确保启动函数被调用
-	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", now.Ip, now.Port))
 	if err != nil {
 		fmt.Println("net.listen err:", err)
 		return
 	}
-	fmt.Printf("服务器已启动，监听中：%s:%d\n", this.Ip, this.Port)
+	fmt.Printf("服务器已启动，监听中：%s:%d\n", now.Ip, now.Port)
 	defer listener.Close()
+	go now.ListenMessager()
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("listener accept err:", err)
 			continue
 		}
-		go this.Handler(conn)
+		go now.Handler(conn)
 	}
 }
